ab3efab63a74d164c96b7144fdee38068212147c
Pawel Czuchaj <pawlo1501@gmail.com>
2015-04-22 22:33:36 +0200

    pawlo1501/mozenie: wygenerowana klasa - phpspec

---
+++ spec/pawlo1501/Tools/mnozenieSpec.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace spec\pawlo1501\Tools;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class mnozenieSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('pawlo1501\Tools\mnozenie');
+    }
+}
