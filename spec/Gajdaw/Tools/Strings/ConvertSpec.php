caa6168ee132719a9c4f3fb4c718f9fcaa7c2f0f
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-04-23 08:49:50 +0200

    Gajdaw: toLower - class Convert

---
+++ spec/Gajdaw/Tools/Strings/ConvertSpec.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace spec\Gajdaw\Tools\Strings;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ConvertSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Gajdaw\Tools\Strings\Convert');
+    }
+
+    function it_should_contain_setter_and_getter_for_a_string()
+    {
+        $this->setStr('lorem')->getStr()->shouldReturn('lorem');
+    }
+
+    function it_should_convert_string_to_lower_case()
+    {
+        $this->setStr('LoReM')->toLower()->shouldReturn('lorem');
+    }
+}
---
+++ src/Gajdaw/Tools/Strings/Convert.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Gajdaw\Tools\Strings;
+
+class Convert
+{
+    private $str;
+
+    public function setStr($str)
+    {
+        $this->str = $str;
+
+        return $this;
+    }
+
+    public function getStr()
+    {
+        return $this->str;
+    }
+
+    public function toLower()
+    {
+        return strtolower($this->str);
+    }
+}
