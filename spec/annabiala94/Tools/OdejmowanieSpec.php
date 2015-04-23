1f267e35ce41ab2d528bfcdd843658823e4217b8
Anna Biala <aniamuzyka121@wp.pl>
2015-04-23 22:43:01 +0200

    annabiala94/odejmowanie: wygenerowana klasa - phpspec

---
+++ spec/annabiala94/Tools/OdejmowanieSpec.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace spec\annabiala94\Tools;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class OdejmowanieSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('annabiala94\Tools\Odejmowanie');
+    }
+}
