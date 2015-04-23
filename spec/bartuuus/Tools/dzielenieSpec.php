d41694ce1471a6e7deaf5d6addde90063b8e1a1d
Bartlomiej Paziewski <bartus.paziewski@gmail.com>
2015-04-23 12:26:43 +0200

    BartuuuS/dzielenie: wygenerowana klasa - phpspec

---
+++ spec/bartuuus/Tools/dzielenieSpec.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace spec\bartuuus\Tools;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class dzielenieSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('bartuuus\Tools\dzielenie');
+    }
+}
