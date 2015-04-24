f232e46c164f184c0f0e2a0c7f55f9fc3d70a78e
GojiraGreen <gojira.green@gmail.com>
2015-04-24 10:18:40 +0200

    kalkulator klasa i metody

--- spec/gojira/Tools/KalkulatorSpec.php
+++ spec/gojira/Tools/KalkulatorSpec.php
@@ -11,4 +11,24 @@ class KalkulatorSpec extends ObjectBehavior
     {
         $this->shouldHaveType('gojira\Tools\Kalkulator');
     }
+
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+    }
+    
+    function it_should_calculate_sum()
+    {
+        $this->setA(5)->setB(2)->sum()->shouldReturn(7);
+    }
+    
+    function it_should_calculate_subtraction()
+    {
+        $this->setA(10)->setB(5)->subtraction()->shouldReturn(5);
+    }
+    function it_should_calculate_multiplication()
+    {
+        $this->setA(5)->setB(5)->multiplication()->shouldReturn(25);
+    }
 }
