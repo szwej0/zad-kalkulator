5d760b078ca2496c45c1ab11c3ed37d7b424b637
kzolnierz <zolnierz.93@gmail.com>
2015-05-01 13:51:14 +0200

    MnozenieCD

--- spec/kzolnierz/Moje/kalkmnoz/MnozenieSpec.php
+++ spec/kzolnierz/Moje/kalkmnoz/MnozenieSpec.php
@@ -11,4 +11,15 @@ class MnozenieSpec extends ObjectBehavior
     {
         $this->shouldHaveType('kzolnierz\Moje\kalkmnoz\Mnozenie');
     }
+    
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+    }
+    
+    function it_should_calculate_multiplication()
+    {   
+    $this->setA(2)->setB(2)->multiplication()->shouldReturn(4);
+    }
 }
--- src/kzolnierz/Moje/kalkmnoz/Mnozenie.php
+++ src/kzolnierz/Moje/kalkmnoz/Mnozenie.php
@@ -4,4 +4,36 @@ namespace kzolnierz\Moje\kalkmnoz;
 
 class Mnozenie
 {
+
+    private $a;
+    private $b;
+
+    public function setA($a)
+    {
+        $this->a = $a;
+
+        return $this;
+    }
+
+    public function setB($b)
+    {
+        $this->b = $b;
+
+        return $this;
+    }
+
+    public function getA()
+    {
+        return $this->a;
+    }
+
+    public function getB()
+    {
+        return $this->b;
+    }
+
+    public function multiplication()
+    {
+        return $this->a * $this->b;
+    }
 }
