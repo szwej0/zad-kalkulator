cb7e9088f9b06bd23f94ed6789d8fa076c62d08b
Rafal Nawalany <rafalnav@gmail.com>
2015-05-01 21:54:12 +0200

    RafalNav/Kalkulator

--- spec/rafalnav/Tools/KalkulatorSpec.php
+++ spec/rafalnav/Tools/KalkulatorSpec.php
@@ -11,4 +11,13 @@ class KalkulatorSpec extends ObjectBehavior
     {
         $this->shouldHaveType('rafalnav\Tools\Kalkulator');
     }
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+    }
+    function it_should_calculate_sum()
+    {
+        $this->setA(5)->setB(2)->sum()->shouldReturn(7);
+    }
 }
