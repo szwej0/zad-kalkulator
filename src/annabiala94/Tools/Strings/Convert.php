d994c4ce0b880cb6f8ba69053ecebc3902d7d180
Anna Biala <aniamuzyka121@wp.pl>
2015-05-03 21:05:18 +0200

    strtolower

--- src/annabiala94/Tools/Strings/Convert.php
+++ src/annabiala94/Tools/Strings/Convert.php
@@ -4,4 +4,23 @@ namespace annabiala94\Tools\Strings;
 
 class Convert
 {
+   private $str;
+   
+    public function setStr($str)
+    {
+        $this->str = $str;
+
+        return $this;
+    }
+
+    public function getStr()
+    {
+        return $this->str;
+    }
+
+    public function toLower()
+    {
+        return strtolower($this->str);
+    }
 }
+
