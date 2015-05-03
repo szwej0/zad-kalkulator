ccc05268e1eeb4417559d56f85fcbe3240d39c05
mmajewski1313 <mmajewski1313@gmail.com>
2015-05-03 12:40:53 +0200

    duze na male- projekt

---
+++ spec/Mmajewski1313/Tools/Strings/ConvertSpec.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace spec\Mmajewski1313\Tools\Strings;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ConvertSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Mmajewski1313\Tools\Strings\Convert');
+    }
+}
