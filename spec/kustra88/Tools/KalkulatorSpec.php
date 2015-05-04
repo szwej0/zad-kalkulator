3aea688b42c29abf9f9744cbd79ee2c0cb0594dc
PiotrekKustra <piotrek.kustra@hotmail.com>
2015-05-04 21:34:59 +0200

    Kalkulator - dodanie metody add()

--- spec/kustra88/Tools/KalkulatorSpec.php
+++ spec/kustra88/Tools/KalkulatorSpec.php
@@ -11,4 +11,13 @@ class KalkulatorSpec extends ObjectBehavior
     {
         $this->shouldHaveType('kustra88\Tools\Kalkulator');
     }
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+    }
+	function it_should_add()
+	{
+	    $this->setA(5)->setB(7)->add()->shouldReturn(12);
+	}
 }
