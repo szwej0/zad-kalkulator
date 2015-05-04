ed16936b5d81842af1219272ea95bd90c9a46023
kzolnierz <zolnierz.93@gmail.com>
2015-05-04 16:45:48 +0200

    toLower

--- spec/kzolnierz/Tools/Strings/ConvertSpec.php
+++ spec/kzolnierz/Tools/Strings/ConvertSpec.php
@@ -11,4 +11,14 @@ class ConvertSpec extends ObjectBehavior
     {
         $this->shouldHaveType('kzolnierz\Tools\Strings\Convert');
     }
+    
+    function it_should_contain_setter_and_getter_for_a_string()
+    {
+        $this->setStr('lorem')->getStr()->shouldReturn('lorem');
+    }
+    
+     function it_should_convert_string_to_lower_case()
+    {
+        $this->setStr('LoReM')->toLower()->shouldReturn('lorem');
+    }
 }
--- src/kzolnierz/Tools/Strings/Convert.php
+++ src/kzolnierz/Tools/Strings/Convert.php
@@ -4,4 +4,19 @@ namespace kzolnierz\Tools\Strings;
 
 class Convert
 {
+    private $str;
+   
+    public function setStr($str)
+    {
+        $this->str = $str;
+        return $this;
+    }
+    public function getStr()
+    {
+        return $this->str;
+    }
+    public function toLower()
+    {
+        return strtolower($this->str);
+    }
 }
