28a793d4d438301cf2909351476eb558193b6251
FurtakM <serpentow@gmail.com>
2015-05-05 00:46:01 +0200

    Metoda Add()

--- spec/FurtakM/Tools/KalkulatorSpec.php
+++ spec/FurtakM/Tools/KalkulatorSpec.php
@@ -11,4 +11,20 @@ class KalkulatorSpec extends ObjectBehavior
     {
         $this->shouldHaveType('FurtakM\Tools\Kalkulator');
     }
+    
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+    }
+    
+    function it_should_calculate_sum()
+    {
+        $this->setA(5)->setB(7)->sum()->shouldReturn(12);
+    }
+    
+    function my_function_add()
+    {
+        $this->add(5,6)->shouldReturn(11);
+    }
 }
--- src/FurtakM/Tools/Kalkulator.php
+++ src/FurtakM/Tools/Kalkulator.php
@@ -4,4 +4,41 @@ namespace FurtakM\Tools;
 
 class Kalkulator
 {
+
+    private $a;
+    private $b;
+
+    public function setA($a)
+    {
+        $this->a = $a;
+
+        return $this;
+    }
+    
+    public function add($a, $b)
+    {
+       return $this->$a+$b;
+    }
+
+    public function setB($b)
+    {
+        $this->b = $b;
+
+        return $this;
+    }
+
+    public function getA()
+    {
+        return $this->a;
+    }
+
+    public function getB()
+    {
+        return $this->b;
+    }
+
+    public function sum()
+    {
+        return $this->a + $this->b;
+    }
 }
