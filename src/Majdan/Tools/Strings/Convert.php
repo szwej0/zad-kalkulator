54d6ebd94602d748089cd4e6ebd2d8498e589d7c
majdan <majdanski.dawid@gmail.com>
2015-05-05 21:18:32 +0200

    toLower

---
+++ src/Majdan/Tools/Strings/Convert.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Majdan\Tools\Strings;
+
+class Convert
+{
+
+	private $str;
+
+    public function setStr($str)
+    {
+        $this->str = $str;
+
+        return $this;
+    }
+
+    public function getStr()
+    {
+        return $this->str;
+    }
+
+    public function toLower()
+    {
+        return strtolower($this->str);
+    }
+    
+}
