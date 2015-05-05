6f50743f2318aba0948d37d0b39e9b66ec7a2a9a
Pawel Czuchaj <pawlo1501@gmail.com>
2015-05-05 19:32:40 +0200

    Kowersja stringow

---
+++ src/pawlo1501/Tools/Strings/Convert.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace pawlo1501\Tools\Strings;
+
+class Convert
+{
+   private $str;
+   
+    public function setStr($str)
+    {
+        $this->str = $str;
+        return $this;
+    }
+    public function getStr()
+    {
+        return $this->str;
+    }
+    public function toLower()
+    {
+        return strtolower($this->str);
+    }
+    public function toUpper()
+    {
+        return strtoupper($this->str);
+    }
+    public function ucfirst()
+    {
+        return ucfirst($this->str);
+    }
+}
