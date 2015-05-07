9214d420c8a89eafa50622590b6c968b4f67b036
michalli <mikemystify@gmail>
2015-05-07 22:36:21 +0200

    michalli/kalkulator: setter/getter

--- spec/michalli/Tools/KalkulatorSpec.php
+++ spec/michalli/Tools/KalkulatorSpec.php
@@ -11,4 +11,10 @@ class KalkulatorSpec extends ObjectBehavior
     {
         $this->shouldHaveType('michalli\Tools\Kalkulator');
     }
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+    }
+
 }
