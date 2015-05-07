bbcb93853e8f686cf18819a5cf65d7fcda4a06e5
Konrad Halasa <konrad.ha@o2.pl>
2015-05-07 22:29:47 +0200

    Convert

---
+++ src/Konradha9/Tools/Strings/Convert.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Konradha9\Tools\Strings;
+
+class Convert
+{
+    private $str;
+   
+    public function setStr($str)
+    {
+        $this->str = $str;
+        return $this;
+    }
+    public function getStr()
+    {
+        return $this->str;
+    }
+    public function toLower()
+    {
+        return strtolower($this->str);
+    }
+    
+    public function toUpper()
+    {
+        return strtoupper($this->str);
+    }
+    
+    public function ucfirst()
+    {
+        return ucfirst($this->str);
+    }
+}
