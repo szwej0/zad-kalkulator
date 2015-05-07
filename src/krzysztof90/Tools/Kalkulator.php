a5151f7c64408c57f2dfd545d7dcdd1d3aed1600
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-05-07 09:24:46 +0200

    Revert "kalkulator klasa convert"
    This reverts commit c9f6ed11e28a9df8b8d955479b5683bc528af61a.

--- src/krzysztof90/Tools/Kalkulator.php
+++ src/krzysztof90/Tools/Kalkulator.php
@@ -4,48 +4,4 @@ namespace krzysztof90\Tools;
 
 class Kalkulator
 {
-	
-    private $a;
-    private $b;
-
-    public function setA($a)
-    {
-        $this->a = $a;
-
-        return $this;
-    }
-
-    public function setB($b)
-    {
-        $this->b = $b;
-
-        return $this;
-    }
-
-
-    public function sum()
-    {
-        return $this->a + $this->b;
-    }
-		
-    public function multiplicate()
-    {
-        return $this->a * $this->b;
-    }
-		
-		public function subtract()
-    {
-        return $this->a - $this->b;
-    }
-
-    public function getA()
-    {
-        return $this->a;
-    }
-
-    public function getB()
-    {
-        return $this->b;
-    }
-
 }
