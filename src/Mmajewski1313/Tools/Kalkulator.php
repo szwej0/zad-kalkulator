b317a58745a0b5036b91b428cd227f21d36f7ed2
mmajewski1313 <mmajewski1313@gmail.com>
2015-05-13 22:31:02 +0200

    Poprawy w kalkulatorze i zmiany w zamianach liter

--- src/Mmajewski1313/Tools/Kalkulator.php
+++ src/Mmajewski1313/Tools/Kalkulator.php
@@ -4,4 +4,40 @@ namespace Mmajewski1313\Tools;
 
 class Kalkulator
 {
+    private $a;
+    private $b;
+
+    public function setA($a)
+    {
+        $this->a = $a;
+
+        return $this;
+    }
+    
+    public function add($a, $b)
+    {
+       return $this->$a+$b;
+    }
+
+    public function setB($b)
+    {
+        $this->b = $b;
+
+        return $this;
+    }
+
+    public function getA()
+    {
+        return $this->a;
+    }
+
+    public function getB()
+    {
+        return $this->b;
+    }
+
+    public function sum()
+    {
+        return $this->a + $this->b;
+    }
 }
--- src/Mmajewski1313/Tools/Strings/Convert.php
+++ src/Mmajewski1313/Tools/Strings/Convert.php
@@ -4,4 +4,29 @@ namespace Mmajewski1313\Tools\Strings;
 
 class Convert
 {
+    private $str;
+   
+    public function setStr($str)
+    {
+        $this->str = $str;
+        return $this;
+    }
+    public function getStr()
+    {
+        return $this->str;
+    }
+    public function toLower()
+    {
+        return strtolower($this->str);
+    }
+    
+    public function toUpper()
+    {
+        return strtoupper($this->str);
+    }
+    
+    public function ucfirst()
+    {
+        return ucfirst($this->str);
+    }
 }
