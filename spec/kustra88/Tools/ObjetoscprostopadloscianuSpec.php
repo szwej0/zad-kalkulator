78bfd12317e920ffacd0ace5ad75400d0758cb33
PiotrekKustra <piotrek.kustra@hotmail.com>
2015-05-14 08:48:06 +0200

    kustra88/objetoscProstopadloscianu: implementacja metod

--- spec/kustra88/Tools/ObjetoscprostopadloscianuSpec.php
+++ spec/kustra88/Tools/ObjetoscprostopadloscianuSpec.php
@@ -11,4 +11,14 @@ class ObjetoscprostopadloscianuSpec extends ObjectBehavior
     {
         $this->shouldHaveType('kustra88\Tools\Objetoscprostopadloscianu');
     }
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(2)->getA()->shouldReturn(2);
+        $this->setB(2)->getB()->shouldReturn(2);
+        $this->setH(2)->getH()->shouldReturn(2);
+    }
+    function it_should_calculate_objetosc()
+	{
+	    $this->setA(2)->setB(2)->setH(2)->objetosc()->shouldReturn(8);
+	}
 }
