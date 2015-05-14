032554ef286008f583f1d74e60371c7be35e4fa9
Krzysztof Zolnierz <zolnierz.93@gmail.com>
2015-05-14 20:31:06 +0200

    kzolnierz/objprostopadloscianu: wygenerowana klasa - phpspec

---
+++ spec/kzolnierz/Tools/ObjprostopadloscianuSpec.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace spec\kzolnierz\Tools;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ObjprostopadloscianuSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('kzolnierz\Tools\Objprostopadloscianu');
+    }
+}
