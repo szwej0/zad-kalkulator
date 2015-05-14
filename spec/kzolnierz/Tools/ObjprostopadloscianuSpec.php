e014e636ae58cac7af528d102922d3df82eece73
Krzysztof Zolnierz <zolnierz.93@gmail.com>
2015-05-14 20:41:07 +0200

    kzolnierz/objprostopadloscianu: CD

--- spec/kzolnierz/Tools/ObjprostopadloscianuSpec.php
+++ spec/kzolnierz/Tools/ObjprostopadloscianuSpec.php
@@ -11,4 +11,16 @@ class ObjprostopadloscianuSpec extends ObjectBehavior
     {
         $this->shouldHaveType('kzolnierz\Tools\Objprostopadloscianu');
     }
+    
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(4)->getA()->shouldReturn(4);
+        $this->setB(3)->getB()->shouldReturn(3);
+        $this->setC(2)->getC()->shouldReturn(2);
+    }
+    
+    function it_should_calculate_obj()
+{
+    $this->setA(4)->setB(3)->setC(2)->obj()->shouldReturn(24);
+}
 }
