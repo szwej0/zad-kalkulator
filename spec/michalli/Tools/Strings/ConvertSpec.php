a7bfdccd1fada2b70dbd19b95d3bdb298cd30e2d
michalli <mikemystify@gmail>
2015-05-14 19:40:08 +0200

    michalli/konwersja: implementacja

--- spec/michalli/Tools/Strings/ConvertSpec.php
+++ spec/michalli/Tools/Strings/ConvertSpec.php
@@ -11,4 +11,24 @@ class ConvertSpec extends ObjectBehavior
     {
         $this->shouldHaveType('michalli\Tools\Strings\Convert');
     }
+    
+    function it_should_contain_setter_and_getter_for_a_string()
+    {
+        $this->setStr('lorem')->getStr()->shouldReturn('lorem');
+    }
+    
+     function it_should_convert_string_to_lower_case()
+    {
+        $this->setStr('LoReM')->toLower()->shouldReturn('lorem');
+    }
+    
+     function it_should_convert_string_to_upper_case()
+    {
+        $this->setStr('LoReM')->toUpper()->shouldReturn('LOREM');
+    }
+    
+    function it_should_convert_uc_first_case()
+    {
+        $this->setStr('lorem')->ucfisrt()->shouldReturn('Lorem');
+    }
 }
