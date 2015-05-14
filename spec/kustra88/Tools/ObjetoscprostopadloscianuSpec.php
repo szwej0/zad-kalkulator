613bfbb87e5bbca3f18513f5e25fde317d13c9a0
PiotrekKustra <piotrek.kustra@hotmail.com>
2015-05-14 08:43:00 +0200

    kustra88/objetoscProstopadloscianu: wygenerowana klasa - phpspec

---
+++ spec/kustra88/Tools/ObjetoscprostopadloscianuSpec.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace spec\kustra88\Tools;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ObjetoscprostopadloscianuSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('kustra88\Tools\Objetoscprostopadloscianu');
+    }
+}
