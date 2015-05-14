fc3bb5074750a06768458fb033006bb57b0b69ab
PiotrekKustra <piotrek.kustra@hotmail.com>
2015-05-14 07:55:23 +0200

    kustra88/polekwadratu: implementacja metod

--- spec/kustra88/Tools/PolekwadratuSpec.php
+++ spec/kustra88/Tools/PolekwadratuSpec.php
@@ -11,4 +11,12 @@ class PolekwadratuSpec extends ObjectBehavior
     {
         $this->shouldHaveType('kustra88\Tools\Polekwadratu');
     }
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(2)->getA()->shouldReturn(2);
+    }
+	function it_should_calculate_polekwadratu()
+	{
+	    $this->setA(2)->polekwadratu()->shouldReturn(4);
+	}
 }
