c50a6060b78612f3d700834c7abc5cde01d6c618
Kamil Chmielowski <kamil.chmielowski@hotmail.com>
2015-05-14 23:59:12 +0200

    kalkulatorcd metoda add()

--- src/szwej0/tools/kalkulatormnozenie.php
+++ src/szwej0/tools/kalkulatormnozenie.php
@@ -3,5 +3,44 @@
 namespace szwej0\tools;
 
 class kalkulatormnozenie
+
 {
+
+    private $a;
+    private $b;
+
+    public function setA($a)
+    {
+        $this->a = $a;
+
+        return $this;
+    }
+
+    public function setB($b)
+    {
+        $this->b = $b;
+
+        return $this;
+    }
+
+    public function getA()
+    {
+        return $this->a;
+    }
+
+    public function getB()
+    {
+        return $this->b;
+    }
+    public function add()
+    {
+        return $this->a * $this->b;
+    }
+
+    function it_should_calculate_add()
+    {
+        $this->setA(2)->setB(3)->add()->shouldReturn(6);
+    }
+
+
 }
