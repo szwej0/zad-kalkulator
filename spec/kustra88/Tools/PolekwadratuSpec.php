78fa7036675740deaea393d645beb4096e213855
PiotrekKustra <piotrek.kustra@hotmail.com>
2015-05-14 07:49:09 +0200

    kustra88/polekwadratu: wygenerowana klasa - phpspec

---
+++ spec/kustra88/Tools/PolekwadratuSpec.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace spec\kustra88\Tools;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class PolekwadratuSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('kustra88\Tools\Polekwadratu');
+    }
+}
