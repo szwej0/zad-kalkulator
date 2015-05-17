7d10b4b86448cdea1a8e7768fca99431c7aef558
Bartlomiej Paziewski <bartus.paziewski@gmail.com>
2015-05-17 21:24:53 +0200

    bartuuusKalkulator: metoda Add()

--- src/bartuuus/Tools/Kalkulator.php
+++ src/bartuuus/Tools/Kalkulator.php
@@ -4,4 +4,33 @@ namespace bartuuus\Tools;
 
 class Kalkulator
 {
+
+    public function setA($a)
+    {
+        $this->a = $a;
+
+        return $this;
+    }
+
+    public function getA()
+    {
+        return $this->a;
+    }
+
+    public function setB($b)
+    {
+        $this->b = $b;
+
+        return $this;
+    }
+
+    public function getB()
+    {
+        return $this->b;
+    }
+
+    public function sum()
+    {
+        return $this->a + $this->b;
+    }
 }
