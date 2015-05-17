7bb7881aa2411fca47a0c88555655af63ebb7bb8
kamilstrzelecki <kstrzelecki@outlook.com>
2015-05-17 20:47:24 +0200

    klasa kalkulator: getery, setery, add

--- spec/kamilstrzelecki/Tools/kalkulator/KalkulatorSpec.php
+++ spec/kamilstrzelecki/Tools/kalkulator/KalkulatorSpec.php
@@ -11,4 +11,16 @@ class KalkulatorSpec extends ObjectBehavior
     {
         $this->shouldHaveType('kamilstrzelecki\Tools\kalkulator\Kalkulator');
     }
+    
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setX(25)->getX()->shouldReturn(25);
+        $this->setY(26)->getY()->shouldReturn(26);
+        $this->setZ(27)->getZ()->shouldReturn(27);
+    }
+    
+    function it_should_calculate_add()
+    {
+        $this->setX(2)->setY(3)->setZ(4)->add()->shouldReturn(9);
+    }
 }
--- src/kamilstrzelecki/Tools/kalkulator/Kalkulator.php
+++ src/kamilstrzelecki/Tools/kalkulator/Kalkulator.php
@@ -4,4 +4,47 @@ namespace kamilstrzelecki\Tools\kalkulator;
 
 class Kalkulator
 {
+
+    public $x;
+    public $y;
+    public $z;
+
+
+    public function setX($x)
+    {
+        $this->x = $x;
+        return $this;
+    }
+
+    public function getX()
+    {
+        return $this->x;
+    }
+
+    public function setY($y)
+    {
+        $this->y = $y;
+        return $this;
+    }
+
+    public function getY()
+    {
+        return $this->y;
+    }
+
+    public function setZ($z)
+    {
+        $this->z = $z;
+        return $this;
+    }
+
+    public function getZ()
+    {
+        return $this->z;
+    }
+
+    public function add()
+    {
+        return $this->x + $this->y + $this->z;
+    }
 }
