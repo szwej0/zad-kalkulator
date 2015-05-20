31359feb27ce27b946349f5c9f455b3df94af1f6
palina888 <palina888@o2.pl>
2015-05-20 09:39:55 +0200

    palina888/prostopadloscian:settery i gettery

--- spec/palina888/Tools/ProstopadloscianSpec.php
+++ spec/palina888/Tools/ProstopadloscianSpec.php
@@ -11,4 +11,16 @@ class ProstopadloscianSpec extends ObjectBehavior
     {
         $this->shouldHaveType('palina888\Tools\Prostopadloscian');
     }
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+        $this->setH(456)->getH()->shouldReturn(456);
+    }
+    
+    function it_should_calculate_prostopadloscian()
+    {
+        $this->setA(3)->setB(4)->setH(5)->prostopadloscian()->shouldReturn(60);
+    }
+
 }
