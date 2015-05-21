3b644c6c710aa061aa8a8b4be3de2a9d1c04dd3e
kamilstrzelecki <kstrzelecki@outlook.com>
2015-05-21 15:36:02 +0200

    objetosc prostopadloscianu: metody

--- spec/kamilstrzelecki/Tools/CuboidSpec.php
+++ spec/kamilstrzelecki/Tools/CuboidSpec.php
@@ -11,4 +11,16 @@ class CuboidSpec extends ObjectBehavior
     {
         $this->shouldHaveType('kamilstrzelecki\Tools\Cuboid');
     }
+    
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(23)->getA()->shouldReturn(23);
+        $this->setB(15)->getB()->shouldReturn(15);
+        $this->setH(18)->getH()->shouldReturn(18);
+    }
+    
+    function it_should_calculate_volume()
+    {
+        $this->setA(3)->setB(4)->setH(5)->volume()->shouldReturn(60);
+    }
 }
