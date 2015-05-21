b9c04c8833abded90b6c4d8b0a7cce07828f8952
Kamil Strzelecki <kstrzelecki@outlook.com>
2015-05-21 08:24:00 +0200

    pole kwadratu: metody

--- spec/kamilstrzelecki/Tools/SquareSpec.php
+++ spec/kamilstrzelecki/Tools/SquareSpec.php
@@ -11,4 +11,14 @@ class SquareSpec extends ObjectBehavior
     {
         $this->shouldHaveType('kamilstrzelecki\Tools\Square');
     }
+    
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(23)->getA()->shouldReturn(23);
+    }
+    
+    function it_should_calculate_area()
+    {
+        $this->setA(4)->area()->shouldReturn(16);
+    }
 }
