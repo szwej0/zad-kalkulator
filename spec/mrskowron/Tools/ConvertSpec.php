1a07df8760b9b0e3ef9d3c4b6e6eb91b0edfc43b
Michal Skowron <mrskowron@student.kul.lublin.pl>
2015-05-25 19:02:19 +0200

    mrskowron/Convert:metody

--- spec/mrskowron/Tools/ConvertSpec.php
+++ spec/mrskowron/Tools/ConvertSpec.php
@@ -11,4 +11,21 @@ class ConvertSpec extends ObjectBehavior
     {
         $this->shouldHaveType('mrskowron\Tools\Convert');
     }
+    
+    function it_should_contain_setter_and_getter_for_a_string()
+    {
+        $this->setStr('lorem')->getStr()->shouldReturn('lorem');
+    }
+    function it_should_convert_string_to_lower_case()
+    {
+        $this->setStr('IPSUM')->toLower()->shouldReturn('ipsum');
+    }
+    function it_should_convert_string_to_upper_case()
+    {
+        $this->setStr('lorem')->toUpper()->shouldReturn('LOREM');
+    }
+    function it_should_convert_string_to_capitalize()
+    {
+        $this->setStr('ipsum')->capitalize()->shouldReturn('Ipsum');
+    }
 }
--- src/mrskowron/Tools/Convert.php
+++ src/mrskowron/Tools/Convert.php
@@ -4,4 +4,31 @@ namespace mrskowron\Tools;
 
 class Convert
 {
+
+    public function setStr($str)
+    {
+        $this->str = $str;
+        
+        return $this;
+    }
+
+    public function getStr()
+    {
+        return $this->str;
+    }
+    
+    public function toLower()
+    {
+        return strtolower($this->str);
+    }
+    
+    public function toUpper()
+    {
+        return strtoupper($this->str);
+    }
+    
+    public function capitalize()
+    {
+        return ucfirst($this->str);
+    }
 }
