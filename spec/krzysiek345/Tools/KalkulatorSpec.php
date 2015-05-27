ada46212fd7c565ab572fb7b44f21c6f378b6442
Krzysztof Pastucha <krzysiek_345@wp.pl>
2015-05-28 00:54:45 +0200

    kalkulator

--- spec/krzysiek345/Tools/KalkulatorSpec.php
+++ spec/krzysiek345/Tools/KalkulatorSpec.php
@@ -11,4 +11,15 @@ class KalkulatorSpec extends ObjectBehavior
     {
         $this->shouldHaveType('krzysiek345\Tools\Kalkulator');
     }
+
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(5)->getA()->shouldReturn(5);
+        $this->setB(7)->getB()->shouldReturn(7);
+    }
+    
+    function it_should_calculate_sum()
+    {
+        $this->setA(5)->setB(7)->sum()->shouldReturn(12);
+    }
 }
