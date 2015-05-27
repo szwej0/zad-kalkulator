90b31f24eff2cfa42e08783685fc923e60c739c0
kgrzech <ambros1112@hotmail.com>
2015-05-27 14:24:05 +0200

    kgrzech/kalkulator: pierwszy

--- spec/kgrzech/Tools/KalkulatorSpec.php
+++ spec/kgrzech/Tools/KalkulatorSpec.php
@@ -11,4 +11,9 @@ class KalkulatorSpec extends ObjectBehavior
     {
         $this->shouldHaveType('kgrzech\Tools\Kalkulator');
     }
+     function it_should_have_setter_and_gettter()
+    {
+        $this->setA(5)->getA()->shouldReturn(5);
+        $this->setB(7)->getB()->shouldReturn(7);
+    }
 }
