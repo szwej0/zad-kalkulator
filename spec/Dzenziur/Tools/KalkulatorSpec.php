79807ced71b63b738686ebfe612e2c244efc22d3
dzenziur <dzenziur@gmail.com>
2015-05-28 23:18:35 +0200

    Implementacja metod

--- spec/Dzenziur/Tools/KalkulatorSpec.php
+++ spec/Dzenziur/Tools/KalkulatorSpec.php
@@ -11,4 +11,13 @@ class KalkulatorSpec extends ObjectBehavior
     {
         $this->shouldHaveType('Dzenziur\Tools\Kalkulator');
     }
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+    }
+    function add()
+    {
+        $this->setA(9)->setB(3)->add()->shouldReturn(12);
+    }
 }
